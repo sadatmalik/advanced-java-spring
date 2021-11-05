a564c50bdc2cce42f967b5d6b40ee4bd9b9b6d87
sadat.malik <sadat.malik@me.com>
2021-11-05 20:54:01 +0000

    Implementing testGetAllMoviesSuccess() and testGetAllMoviesFailure(). Closes #2

--- src/test/java/platform/codingnomads/co/lab/MovieControllerTest.java
+++ src/test/java/platform/codingnomads/co/lab/MovieControllerTest.java
@@ -2,15 +2,27 @@ package platform.codingnomads.co.lab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import platform.codingnomads.co.springtest.lab.SpringTestLab;
+import platform.codingnomads.co.springtest.lab.controller.MovieController;
 import platform.codingnomads.co.springtest.lab.repository.MovieRepository;
+import platform.codingnomads.co.springtest.lab.service.MovieService;
+import platform.codingnomads.co.springtest.lab.service.MovieServiceImpl;
 
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest(classes = SpringTestLab.class)
 @AutoConfigureMockMvc
@@ -19,28 +31,33 @@ public class MovieControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private MovieRepository movieRepo;
-
     @Test
     public void testGetAllMoviesSuccess() throws Exception {
         mockMvc.perform(get("/all"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(handler().handlerType(MovieController.class))
+                .andExpect(handler().method(MovieController.class.getMethod("getAllMovies")))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name").value("The Shawshank Redemption"))
+                .andExpect(jsonPath("$[0].rating").value(9.3))
+                .andExpect(jsonPath("$[1].name").value("The Pursuit of Happyness"))
+                .andExpect(jsonPath("$[1].rating").value(8.0));
+
     }
+
+    @Autowired
+    private MovieRepository movieRepository;
 
     @Test
     public void testGetAllMoviesFailure() throws Exception {
-        movieRepo.deleteAll();
 
-        // Check via Postman - does it return a 404 or just an empty json msg body?
+        movieRepository.deleteAll();
+
         mockMvc.perform(get("/all"))
                 .andDo(print())
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    public void testGetAllMoviesSuccessMockService() {
-
+                .andExpect(status().isOk())
+                .andExpect(content().string(emptyString()));
     }
 }

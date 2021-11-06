1683257fee1435622a8d2ec7d83a8e80c1fc079f
sadat.malik <sadat.malik@me.com>
2021-11-06 12:42:03 +0000

    Creating test class for MovieService. Closes #7

---
+++ src/test/java/platform/codingnomads/co/lab/MovieServiceTest.java
@@ -0,0 +1,24 @@
+package platform.codingnomads.co.lab;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieServiceTest {
+
+    @Test
+    public void getAllMoviesSuccess() {
+    }
+
+    @Test
+    public void getAllMoviesSuccessFailure() {
+    }
+
+    @Test
+    public void getMoviesByMinimumRatingSuccess() {
+    }
+
+    @Test
+    public void getMoviesByMinimumRatingFailure() {
+    }
+}

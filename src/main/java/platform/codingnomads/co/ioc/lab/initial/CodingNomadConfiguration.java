f1d6d4ba95f8729106c6cfbc4b2f7c2973cc4f18
sadat.malik <sadat.malik@me.com>
2022-03-22 14:23:10 +0000

    Formatting builder pattern invocation

--- src/main/java/platform/codingnomads/co/ioc/lab/initial/CodingNomadConfiguration.java
+++ src/main/java/platform/codingnomads/co/ioc/lab/initial/CodingNomadConfiguration.java
@@ -10,26 +10,41 @@ public class CodingNomadConfiguration {
 
     @Bean
     public Framework framework() {
-        return Framework.builder().name("Spring Boot").version("2.5").build();
+        return Framework.builder()
+                .name("Spring Boot")
+                .version("2.5")
+                .build();
     }
 
     @Bean
     public IDE ide() {
-        return IDE.builder().name("IntelliJ IDEA").version("2021.1").build();
+        return IDE.builder()
+                .name("IntelliJ IDEA")
+                .version("2021.1")
+                .build();
     }
 
     @Bean
     public JDK jdk() {
-        return JDK.builder().name("OpenJDK").version("11").build();
+        return JDK.builder()
+                .name("OpenJDK")
+                .version("11")
+                .build();
     }
 
     @Bean
     public Template template() {
-        return Template.builder().name("Thymeleaf").version("3.0.11").build();
+        return Template.builder()
+                .name("Thymeleaf")
+                .version("3.0.11")
+                .build();
     }
 
     @Bean
     public Data data() {
-        return Data.builder().name("JPA").version("2.2.3").build();
+        return Data.builder()
+                .name("JPA")
+                .version("2.2.3")
+                .build();
     }
 }
